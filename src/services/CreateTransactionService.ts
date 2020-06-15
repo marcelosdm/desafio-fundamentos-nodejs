7209d35b6dfec4adc64016362bdaf7a710a0fedf
Marcelo Melo <marcelo@STA-PE05BFYN.scf.local>
2020-06-15 10:26:22 -0300

    :sparkles Finish challenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Invalid transaction type');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    if (type === 'outcome' && total < value) {
+      throw new Error(
+        `It's not allowed to create an outcome transaction with a value greater than the total`,
+      );
+    }
+
+    return transaction;
   }
 }
 
